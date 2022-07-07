f73733cf5cf260a541ea754533fb1da56306525e
Jeferson Santos <jeferson.santos@onfriday.com.br>
2022-07-07 11:44:29 -0300

    Add project files.

---
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,54 @@
+﻿namespace MonkeyFinder.ViewModel;
+public partial class BaseViewModel : INotifyPropertyChanged
+{
+
+    bool isBusy;
+    string title;
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+            isBusy = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private void OnPropertyChanged()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value)
+                return;
+            title = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+                return;
+            isBusy = value;
+            OnPropertyChanged();
+            // Also raise the IsNotBusy property changed
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+
+    public bool IsNotBusy => !IsBusy;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
---
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,42 @@
+﻿using IntelliJ.Lang.Annotations;
+using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+    MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new(); 
+    public Command GetMonkeysCommand { get; }
+    public MonkeysViewModel()
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+
+    async Task GetMonkeysAsync()
+    {
+        if(IsBusy)
+            return;
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+        }
+        catch(Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error", $"Unable to get monkeys:{ex.Message}", "OK");
+}
+finally
+        {
+            IsBusy = false;
+        }
+    }
+}
+
